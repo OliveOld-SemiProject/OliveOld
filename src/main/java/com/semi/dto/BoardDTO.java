918060585f4a121e5c5e64ad2779a13d441d4d14
DESKTOP-3EP64R3\56657 <yu_hosang@naver.com>
2022-12-30 13:59:11 +0900

    Add DTOs

---
+++ src/main/java/com/semi/dto/BoardDTO.java
@@ -0,0 +1,29 @@
+package com.semi.dto;
+
+import lombok.*;
+
+import java.sql.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BoardDTO {
+
+    private Long no;
+    private Integer type;
+    private Long CategoryCode;
+    private String title;
+    private String body;
+    private Long writerMemberNo;
+    private MemberDTO writer;
+    private int count;
+    private Date createDate;
+    private Date modifiedDate;
+    private String status;
+    private List<AttachmentDTO> attachmentList;
+    private faqCategoryDTO faqCategory;      // faq 카테고리 목록 조회용 밑으로 관련 카테고리 추가하면 됨
+
+}
