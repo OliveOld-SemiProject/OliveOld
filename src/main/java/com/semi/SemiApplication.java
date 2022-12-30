6b7830335ae6bab7cf06c0cdbe9c8ec346c2f1ba
DESKTOP-3EP64R3\56657 <yu_hosang@naver.com>
2022-12-30 14:11:03 +0900

    Merge remote-tracking branch 'origin/HS(Hosang)' into HS(Hosang)
    # Conflicts:
    #	src/main/java/com/semi/SemiApplication.java

--- src/main/java/com/semi/SemiApplication.java
+++ src/main/java/com/semi/SemiApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemiApplication {
 
 	public static void main(String[] args) {
+
+
+		System.out.println("Test");
+
 		SpringApplication.run(SemiApplication.class, args);
 	}
+
+
 
 }
