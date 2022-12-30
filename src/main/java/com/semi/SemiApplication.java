ae78bac1293db634de784e0cc75b4ce733637a54
To <tok12225@gmail.com>
2022-12-30 10:49:41 +0900

    commite sample

--- src/main/java/com/semi/SemiApplication.java
+++ src/main/java/com/semi/SemiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemiApplication {
 
 	public static void main(String[] args) {
-
 		System.out.println("slakdjflskajflk");
 		SpringApplication.run(SemiApplication.class, args);
 	}
