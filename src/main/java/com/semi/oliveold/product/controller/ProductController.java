a1a9b39d79d068831b638357d5da6a1f5f058e9e
To <tok12225@gmail.com>
2023-01-12 10:36:15 +0900

    admin 페이지 수정

--- src/main/java/com/semi/oliveold/product/controller/ProductController.java
+++ src/main/java/com/semi/oliveold/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,4 +25,7 @@ public class ProductController {
 
         return "product-Upload";
      }
+
+     @PostMapping("/upload")
+
 }
